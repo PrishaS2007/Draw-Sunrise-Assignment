ece388d8ca575803139319752e0cb3d77f82c3ca
Prisha Shreevastav <112982045+PrishaS2007@users.noreply.github.com>
2023-01-16 22:07:22 -0700

    js

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 let cnv = document.getElementById("myCanvas");
 let ctx = cnv.getContext("2d");
-cnv.width = 600;
+cnv.width = 400;
 cnv.height = 400;
 
-ctx.fillStyle = "blue";
-ctx.fillRect(50, 50, 100, 60);
+let cloud = document.getElementById("cloud");
+
+ctx.fillStyle = "rgb(0, 0, 225)"
+ctx.fillRect(0, 0, 400, 400)
+
+ctx.fillStyle = "red";
+ctx.strokeStyle = "rgb(225, 0, 0)";
+ctx.beginPath();
+ctx.arc(200, 300, 25, 0, 2 * Math.PI);
+ctx.fill();
+
+ctx.strokeStyle = "green";
+ctx.fillStyle = "rgb(0, 225, 0)";
+ctx.fillRect(0, 300, 400, 400);
+
+ctx.drawImage(cloud, 100, 100);
+ctx.drawImage(cloud, 150, 85);
